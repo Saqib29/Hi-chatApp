30e01d3bd51bc4929a43f55a3d0e01200a4a048f
Saqib <saqib29111@gmail.com>
2021-01-12 19:33:07 +0600

    setup socket.io + socket.io in app.js + set client.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,13 +9,18 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 
+
 app.get('/', (req, res) => {
     // console.log(__dirname + '\\public')
     res.sendFile(__dirname + '/index.html');
 });
 
+// Socket 
+io.on('connection', (socket) => {
+    console.log('Connected...');
+});
 
 // server listening...
 http.listen(PORT, () => {
-    console.log(`Server started atr ${PORT}`);
+    console.log(`Server started at ${PORT}`);
 });
