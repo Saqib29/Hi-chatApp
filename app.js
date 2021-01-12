2e7a22a19cb1eecbdcbd45b28e92c75290f04293
Saqib <saqib29111@gmail.com>
2021-01-12 19:20:51 +0600

    frontend part done

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const http = require('http').createServer(app);
 
 const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', (req, res) => {
+    // console.log(__dirname + '\\public')
     res.sendFile(__dirname + '/index.html');
 });
 
+
+// server listening...
 http.listen(PORT, () => {
     console.log(`Server started atr ${PORT}`);
 });
